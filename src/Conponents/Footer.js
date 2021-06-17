faa5578a1ac5debc88d250fc63b65e533e24c4f5
patrick sunday <sundayblinkz1@gmail.com>
2021-06-17 23:15:54 +0100

    updated the footer

--- src/Conponents/Footer.js
+++ src/Conponents/Footer.js
@@ -1,11 +1,30 @@
-import React from 'react'
+import React from "react";
 
 function Footer() {
-    return(
-        <footer className="bg-gray-200 text-center text-xs p-3 absolute bottom-0 w-full">
-            &copy; copyright 2020
-        </footer>
-    )
+  return (
+    <footer className="bg-gray-200 text-center divide-y divide-gray-500 text-xs p-3 absolute bottom-0 w-full">
+      <div className="flex divide-x pb-5 divide-gray-500 justify-between mx-52 px-5">
+        <div className="text-left ">
+          <p className="py-1">Men Fashion</p>
+          <p className="py-1">Women Fashion</p>
+          <p className="py-1">Accesories</p>
+        </div>
+        <div className="pl-48 text-left">
+          <p className="py-1">About Us</p>
+          <p className="py-1">Contact Us</p>
+          <p className="py-1">Location</p>
+        </div>
+        <div className="pl-48 text-left">
+          <p className="py-1">Order And Return</p>
+          <p className="py-1">FaQs</p>
+          <p className="py-1">Search Terms</p>
+        </div>
+      </div>
+      <p className="pt-2">
+        Copyright &copy; {new Date().getFullYear()} | MINI E-COMMERCE.
+      </p>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
--- src/Conponents/Home/Discount.js
+++ src/Conponents/Home/Discount.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Discount = () => {
   return (
-    <div className=" flex flex-col items-center justify-center h-80 w-full bg-transparent ">
+    <div className=" flex flex-col mb-20 items-center justify-center h-80 w-full bg-transparent ">
       <h1 className="text-3xl font-bold">Clearance Sales Up to 80% Off.</h1>
       <h1 className="text-3xl font-bold">All Sales are Final!</h1>
       <p className="text-gray-500">
