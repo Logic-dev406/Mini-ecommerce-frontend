fbcb1a9f78e5ac03b72e9c7dfa83c19d1d04d80a
patrick sunday <sundayblinkz1@gmail.com>
2021-06-18 08:58:32 +0100

    made the footer mobile responsive

--- src/Conponents/Footer.js
+++ src/Conponents/Footer.js
@@ -3,21 +3,21 @@ import React from "react";
 function Footer() {
   return (
     <footer className="bg-gray-200 text-center divide-y divide-gray-500 text-xs p-3 absolute bottom-0 w-full">
-      <div className="flex divide-x pb-5 divide-gray-500 justify-between mx-52 px-5">
+      <div className="flex items-center divide-x pb-5 divide-gray-500 justify-between mx-0 md:mx-52 px-5">
         <div className="text-left ">
-          <p className="py-1">Men Fashion</p>
-          <p className="py-1">Women Fashion</p>
-          <p className="py-1">Accesories</p>
+          <p className="py-0 md:py-1">Men Fashion</p>
+          <p className="py-0 md:py-1">Women Fashion</p>
+          <p className="py-0 md:py-1">Accesories</p>
         </div>
-        <div className="pl-48 text-left">
-          <p className="py-1">About Us</p>
-          <p className="py-1">Contact Us</p>
-          <p className="py-1">Location</p>
+        <div className="pl-5 md:pl-48 pr-5 md:pr-0 text-left">
+          <p className="py-0 md:py-1">About Us</p>
+          <p className="py-0 md:py-1">Contact Us</p>
+          <p className="py-0 md:py-1">Location</p>
         </div>
-        <div className="pl-48 text-left">
-          <p className="py-1">Order And Return</p>
-          <p className="py-1">FaQs</p>
-          <p className="py-1">Search Terms</p>
+        <div className="pl-5 md:pl-48 text-left">
+          <p className="py-0 md:py-1">Order And Return</p>
+          <p className="py-0 md:py-1">FaQs</p>
+          <p className="py-0 md:py-1">Search Terms</p>
         </div>
       </div>
       <p className="pt-2">
